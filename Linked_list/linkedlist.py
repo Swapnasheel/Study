4c1134ac58fa842b9d653adc6490986d91afb2b7
swapnasheel <swar.1318@gmail.com>
2018-02-18 14:06:36 -0800

    Added remove method

--- Linked_list/linkedlist.py
+++ Linked_list/linkedlist.py
@@ -28,6 +28,26 @@ class LinkedList(object):
 		return self.size
 
 
+	def remove(self, data):
+
+		if self.head is None:
+			return
+
+		self.size = self.size - 1
+
+		currentNode = self.head
+		previousNode = None
+
+		while currentNode.data != data:
+			previousNode = currentNode
+			currentNode = currentNode.nextNode
+
+		if previousNode is None:
+			self.head = currentNode.nextNode
+		else:
+			previousNode.nextNode = currentNode.nextNode
+
+
 	# To check size of the linked list with O(n) complexity
 
 	def size2(self):
